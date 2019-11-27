2dada0214771962aa82fbe976467abc90960a6e2
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-11-27 10:21:42 -0300

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-  return (
-    <Title>
-      Main
-      <span className="teste">Pequeno</span>
-    </Title>
-  );
+  return <Title>Hello world!</Title>;
 }
