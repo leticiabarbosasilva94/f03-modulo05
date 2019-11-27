47f82185d444fcd964d396cfd1a93e959ad82f9d
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-11-27 09:22:10 -0300

    Adding eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
   );
 }
 
